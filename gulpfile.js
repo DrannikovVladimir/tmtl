ee7641520a8e9cc084dd398af45c60fa925d505e
Vladimir Drannikov <krowen@inbox.ru>
2023-03-14 14:18:34 +0600

    Added articles

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,52 @@
 const { src, dest, parallel, series, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
+const plumber = require('gulp-plumber');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const cleanCss = require('gulp-clean-css');
+
+//  paths
+const srcPath = "src/";
+const distPath = "dist/";
+
+const path = {
+  build: {
+    html: distPath,
+    css: distPath + "css/",
+    js: distPath + "js/",
+    images: distPath + "img/",
+    fonts: distPath + "fonts/"
+  },
+  src: {
+    html: srcPath + "**/*.html",
+    css: srcPath + "sass/styles.scss",
+    js: srcPath + "js/**/*.js",
+    images: srcPath + "img/**/*.{webp,png,jpeg,jpg,svg,ico}",
+    fonts: srcPath + "fonts/**/*.{woff,woff2,eot,ttf}"
+  },
+  watch: {
+    html: srcPath + "**/*.html",
+    css: srcPath + "sass/**/*.scss",
+    js: srcPath + "js/**/*.js",
+    images: srcPath + "img/**/*.{webp,png,jpeg,jpg,svg,ico}",
+    fonts: srcPath + "fonts/**/*.{woff,woff2,eot,ttf}"
+  },
+  clean: "./" + distPath
+};
+
+// Production
+
+function html() {
+  return src(path.src.html, { base: srcPath })
+    .pipe(plumber())
+    .pipe(dest(path.build.html, { base: srcPath }));
+};
+
+exports.html = html;
+
+
+// Development
 
 function getBrowserSync() {
   browserSync.init({
