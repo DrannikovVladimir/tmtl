1a2c2dbcf912226f3ca7101be46242cb5a2a1a33
Vladimir Drannikov <krowen@inbox.ru>
2023-03-16 14:38:48 +0600

    Added feedback error/success

--- src/js/forms.js
+++ src/js/forms.js
@@ -1,3 +1,9 @@
+const ERRORS = [400, 401, 402, 403, 404, 500];
+const titleBooking = 'Ваша заявка успешно отправлена';
+const titleSub = 'Ваша заявка успешно отправлена';
+const textBooking = 'Спасибо, что выбрали наше турагентство. Мы свяжемся с вами в течении часа и поможем выбрать лучшее предложения для вашего отдыха.';
+const textSub = 'Спасибо, что выбрали наше турагенство. Ваша подписка успешно оформлена.';
+
 const modalFeedback = document.querySelector('.modal-feedback');
 const modalSubs = document.querySelector('.modal-subs');
 
@@ -23,29 +29,114 @@ const serializeData = (data) => {
   return JSON.stringify(Object.fromEntries(formData));
 };
 
-const setDisabledFields = (form) => {
+const setDisabledFieldsBooking = (form) => {
   const nameField = form.querySelector('[name="name"]');
   const phoneField = form.querySelector('[name="phone"]');
   const commentField = form.querySelector('[name="comment"]');
-  const buttonSubmit = form.querySelector('.feedback-form__submit');
 
   nameField.setAttribute('disabled', true);
   phoneField.setAttribute('disabled', true);
   commentField.setAttribute('disabled', true);
-  buttonSubmit.setAttribute('disabled', true);
 };
 
-formFeedback.addEventListener('submit', (e) => {
+const setDisabledFieldsSub = (form) => {
+  const emailField = form.querySelector('[name="email"]');
+
+  emailField.setAttribute('disabled', true);
+};
+
+const renderError = () => {
+  console.log('renderError')
+  const container = document.querySelector('.modal__content--booking');
+  const title = document.createElement('h2');
+  const text = document.createElement('p');
+  const button = document.createElement('button');
+
+  title.classList.add('modal__title');
+  title.textContent = 'Что то пошло не так';
+  button.classList.add('subs-form__submit');
+  button.textContent = 'Закрыть';
+  text.textContent = 'Ошибка при отправке форме. Мы уже разбираемся с этой проблемой. Пожалуйста перезагрузите страницу или попробуйте позжу.'
+  
+  console.log(container, title, text, button);
+  container.innerHTML = '';
+  container.append(title, text, button);
+
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    modalFeedback.classList.add('hidden');
+  });
+};
+
+const renderSuccess = (titleModal, textModal) => {
+  const container = document.querySelector('.modal__content--booking');
+  const title = document.createElement('h2');
+  const text = document.createElement('p');
+  const button = document.createElement('button');
+
+  title.classList.add('modal__title');
+  title.textContent = titleModal;
+  button.classList.add('subs-form__submit');
+  button.textContent = 'Закрыть';
+  text.textContent = textModal;
+  
+  container.innerHTML = '';
+  container.append(title, text, button);
+
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    modalFeedback.classList.add('hidden');
+  });
+};
+
+formFeedback.addEventListener('submit', async (e) => {
   e.preventDefault();
   const data = serializeData(e.target);
-  // setDisabledFields(e.target);
-  console.log(data);
+  setDisabledFieldsBooking(e.target);
+
+  try {
+    const res = await fetch('/api/booking', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: data,
+    });
+    if (ERRORS.includes(res.status)) {
+      console.log('click')
+      renderError();
+      return;
+    }
+    renderSuccess();
+  } catch (error) {
+    console.log(error);
+    renderError();
+  }
 });
 
-formSubs.addEventListener('submit', (e) => {
+formSubs.addEventListener('submit', async (e) => {
   e.preventDefault();
   const data = serializeData(e.target);
-  console.log(data);
+  setDisabledFieldsSub(e.target);
+
+  try {
+    const res = await fetch('/api/sub', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: data,
+    });
+    if (ERRORS.includes(res.status)) {
+      renderError()
+      return;
+    }
+    renderSuccess();
+    modalSubs.classList.add('hidden');
+  } catch (error) {
+    console.log(error);
+    console.log(renderError());
+  }
 });
 
 buttonCloseFeedback.addEventListener('click', (e) => {
