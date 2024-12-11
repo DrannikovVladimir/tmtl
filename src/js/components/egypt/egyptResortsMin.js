99b6eb2856413d8115536e21ec99dc9b53f83dc9
Vladimir <krowen@inbox.ru>
2024-12-11 16:47:48 +0600

    Correced the articles

--- src/js/components/egypt/egyptResortsMin.js
+++ src/js/components/egypt/egyptResortsMin.js
@@ -4,67 +4,70 @@ const egyptResortsMinContainer = ReactDOM.createRoot(
 
 const EgyptResortsMin = () => {
   return (
-    <ul className="article__resorts resorts resorts resorts--list-min">
-      <li className="resorts__item resorts__item--list-min">
-        <a href="/country/egypt/sharm-el-sheikh/" className="resorts__item-link resorts__item-link--list-min">
-          <picture>
-            <source type="image/webp" srcSet="/img/tours/egypt/sharm-el-sheikh-resort.webp" />
-            <img src="/img/tours/egypt/sharm-el-sheikh-resort.jpg" alt="Шарм-Эль-Шейх" className="resorts__item-image resorts__item-image resorts__item-image--list-min" width="250" />
-          </picture>
-          <h3 className="resorts__item-title resorts__item-title--font-size-14 resorts__item-title--list-min">Шарм-Эль-Шейх</h3>
-        </a>
-      </li>
+    <>
+      <h2 className="article__title article__title--list-h3">Курорты Египта</h2>
+      <ul className="article__resorts resorts resorts resorts--list-min">
+        <li className="resorts__item resorts__item--list-min">
+          <a href="/country/egypt/sharm-el-sheikh/" className="resorts__item-link resorts__item-link--list-min">
+            <picture>
+              <source type="image/webp" srcSet="/img/tours/egypt/sharm-el-sheikh-resort.webp" />
+              <img src="/img/tours/egypt/sharm-el-sheikh-resort.jpg" alt="Шарм-Эль-Шейх" className="resorts__item-image resorts__item-image resorts__item-image--list-min" width="250" />
+            </picture>
+            <h3 className="resorts__item-title resorts__item-title--font-size-14 resorts__item-title--list-min">Шарм-Эль-Шейх</h3>
+          </a>
+        </li>
 
-      <li className="resorts__item resorts__item--list-min">
-        <a href="/country/egypt/hurghada/" className="resorts__item-link resorts__item-link--list-min">
-          <picture>
-            <source type="image/webp" srcSet="/img/tours/egypt/hurgada-resort.webp" />
-            <img src="/img/tours/egypt/hurgada-resort.jpg" alt="Хургада" className="resorts__item-image resorts__item-image resorts__item-image--list-min" width="250" />
-          </picture>
-          <h3 className="resorts__item-title resorts__item-title--list-min">Хургада</h3>
-        </a>
-      </li>
+        <li className="resorts__item resorts__item--list-min">
+          <a href="/country/egypt/hurghada/" className="resorts__item-link resorts__item-link--list-min">
+            <picture>
+              <source type="image/webp" srcSet="/img/tours/egypt/hurgada-resort.webp" />
+              <img src="/img/tours/egypt/hurgada-resort.jpg" alt="Хургада" className="resorts__item-image resorts__item-image resorts__item-image--list-min" width="250" />
+            </picture>
+            <h3 className="resorts__item-title resorts__item-title--list-min">Хургада</h3>
+          </a>
+        </li>
 
-      <li className="resorts__item resorts__item--list-min">
-        <a href="/country/egypt/marsa-alam/" className="resorts__item-link resorts__item-link--list-min">
-          <picture>
-            <source type="image/webp" srcSet="/img/tours/egypt/marsa-alam-resort.webp" />
-            <img src="/img/tours/egypt/marsa-alam-resort.jpg" alt="Марса Алам" className="resorts__item-image resorts__item-image resorts__item-image--list-min" width="250" />
-          </picture>
-          <h3 className="resorts__item-title resorts__item-title--list-min">Марса Алам</h3>
-        </a>
-      </li>
+        <li className="resorts__item resorts__item--list-min">
+          <a href="/country/egypt/marsa-alam/" className="resorts__item-link resorts__item-link--list-min">
+            <picture>
+              <source type="image/webp" srcSet="/img/tours/egypt/marsa-alam-resort.webp" />
+              <img src="/img/tours/egypt/marsa-alam-resort.jpg" alt="Марса Алам" className="resorts__item-image resorts__item-image resorts__item-image--list-min" width="250" />
+            </picture>
+            <h3 className="resorts__item-title resorts__item-title--list-min">Марса Алам</h3>
+          </a>
+        </li>
 
-      <li className="resorts__item resorts__item--list-min">
-        <a href="/country/egypt/dahab/" className="resorts__item-link resorts__item-link--list-min">
-          <picture>
-            <source type="image/webp" srcSet="/img/tours/egypt/dahab-resort.webp" />
-            <img src="/img/tours/egypt/dahab-resort.jpg" alt="Дахаб" className="resorts__item-image resorts__item-image resorts__item-image--list-min" width="250" />
-          </picture>
-          <h3 className="resorts__item-title resorts__item-title--list-min">Дахаб</h3>
-        </a>
-      </li>
+        <li className="resorts__item resorts__item--list-min">
+          <a href="/country/egypt/dahab/" className="resorts__item-link resorts__item-link--list-min">
+            <picture>
+              <source type="image/webp" srcSet="/img/tours/egypt/dahab-resort.webp" />
+              <img src="/img/tours/egypt/dahab-resort.jpg" alt="Дахаб" className="resorts__item-image resorts__item-image resorts__item-image--list-min" width="250" />
+            </picture>
+            <h3 className="resorts__item-title resorts__item-title--list-min">Дахаб</h3>
+          </a>
+        </li>
 
-      <li className="resorts__item resorts__item--list-min">
-        <a href="/country/egypt/alexandria/" className="resorts__item-link resorts__item-link--list-min">
-          <picture>
-            <source type="image/webp" srcSet="/img/tours/egypt/alexandria-resort.webp" />
-            <img src="/img/tours/egypt/alexandria-resort.jpg" alt="Александрия" className="resorts__item-image resorts__item-image resorts__item-image--list-min" width="250" />
-          </picture>
-          <h3 className="resorts__item-title resorts__item-title--list-min">Александрия</h3>
-        </a>
-      </li>
+        <li className="resorts__item resorts__item--list-min">
+          <a href="/country/egypt/alexandria/" className="resorts__item-link resorts__item-link--list-min">
+            <picture>
+              <source type="image/webp" srcSet="/img/tours/egypt/alexandria-resort.webp" />
+              <img src="/img/tours/egypt/alexandria-resort.jpg" alt="Александрия" className="resorts__item-image resorts__item-image resorts__item-image--list-min" width="250" />
+            </picture>
+            <h3 className="resorts__item-title resorts__item-title--list-min">Александрия</h3>
+          </a>
+        </li>
 
-      <li className="resorts__item resorts__item--list-min">
-        <a href="/country/egypt/cairo/" className="resorts__item-link resorts__item-link--list-min">
-          <picture>
-            <source type="image/webp" srcSet="/img/tours/egypt/cairo-resort.webp" />
-            <img src="/img/tours/egypt/cairo-resort.jpg" alt="Каир" className="resorts__item-image resorts__item-image resorts__item-image--list-min" width="250" />
-          </picture>
-          <h3 className="resorts__item-title resorts__item-title--list-min">Каир</h3>
-        </a>
-      </li>
-    </ul>
+        <li className="resorts__item resorts__item--list-min">
+          <a href="/country/egypt/cairo/" className="resorts__item-link resorts__item-link--list-min">
+            <picture>
+              <source type="image/webp" srcSet="/img/tours/egypt/cairo-resort.webp" />
+              <img src="/img/tours/egypt/cairo-resort.jpg" alt="Каир" className="resorts__item-image resorts__item-image resorts__item-image--list-min" width="250" />
+            </picture>
+            <h3 className="resorts__item-title resorts__item-title--list-min">Каир</h3>
+          </a>
+        </li>
+      </ul>
+    </>
   )
 };
 
