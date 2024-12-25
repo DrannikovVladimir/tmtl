0b2258f648b8aa6ce9b05c1a0176507c5fc90111
Vladimir Drannikov <krowen@inbox.ru>
2024-12-25 12:08:37 +0500

    Rewrited the article about Hurgada

--- src/js/components/egypt/egyptResorts.js
+++ src/js/components/egypt/egyptResorts.js
@@ -16,7 +16,7 @@ const EgyptResorts = () => {
         </a>
         <div className="resorts__item-content">
           <a href="/country/egypt/sharm-el-sheikh/" className="resorts__item-link">
-            <h3 className="resorts__item-title">Шарм-Эль-Шейх</h3>
+            <h3 id="sharm-el-sheikh" className="resorts__item-title">Шарм-Эль-Шейх</h3>
           </a>
           <p className="resorts__item-text">Шарм-эль-Шейх представляет собой курорт европейского стиля с превосходно развитой туристической инфраструктурой, отельными комплексами и уютными гостиницами. Здесь можно обнаружить как небольшие комфортабельные отели с изысканным современным дизайном.</p>
           <a href="/country/egypt/sharm-el-sheikh/" className="resorts__item-link resorts__item-link--about">Подробнее о регионе</a>
@@ -32,7 +32,7 @@ const EgyptResorts = () => {
         </a>
         <div className="resorts__item-content">
           <a href="/country/egypt/hurghada/" className="resorts__item-link">
-            <h3 className="resorts__item-title">Хургада</h3>
+            <h3 id="hurghada" className="resorts__item-title">Хургада</h3>
           </a>
           <p className="resorts__item-text">Хургада - отличный курорт на берегу Красного моря и представляет собой одно из главных туристических направлений Египта. Он славится своими великолепными песчаными пляжами, живописными коралловыми островами и удивительным разнообразием растительного и животного мира. </p>
           <a href="/country/egypt/hurghada/" className="resorts__item-link resorts__item-link--about">Подробнее о регионе</a>
@@ -48,7 +48,7 @@ const EgyptResorts = () => {
         </a>
         <div className="resorts__item-content">
           <a href="/country/egypt/marsa-alam/" className="resorts__item-link">
-            <h3 className="resorts__item-title">Марса Алам</h3>
+            <h3 id="marsa-alam" className="resorts__item-title">Марса Алам</h3>
           </a>
           <p className="resorts__item-text">Марса Алам находится на расстоянии 300 км к югу от Хургады. Этот быстро развивающийся курорт открыт для приема не только стандартных туристов, но и энтузиастов подводного плавания со всего мира.</p>
           <a href="/country/egypt/marsa-alam/" className="resorts__item-link resorts__item-link--about">Подробнее о регионе</a>
@@ -64,7 +64,7 @@ const EgyptResorts = () => {
         </a>
         <div className="resorts__item-content">
           <a href="/country/egypt/dahab/" className="resorts__item-link">
-            <h3 className="resorts__item-title">Дахаб</h3>
+            <h3 id="dahab" className="resorts__item-title">Дахаб</h3>
           </a>
           <p className="resorts__item-text">Дахаб - это идеальное место для любителей подводных экскурсий, рыбалки и виндсерфинга. Вы получите возможность наблюдать за удивительным подводным миром в живописной Голубой Бухте или таинственном Каньоне - местах, которые особенно любят опытные дайверы.</p>
           <a href="/country/egypt/dahab/" className="resorts__item-link resorts__item-link--about">Подробнее о регионе</a>
@@ -80,7 +80,7 @@ const EgyptResorts = () => {
         </a>
         <div className="resorts__item-content">
           <a href="/country/egypt/alexandria/" className="resorts__item-link">
-            <h3 className="resorts__item-title">Александрия</h3>
+            <h3 id="alexandria" className="resorts__item-title">Александрия</h3>
           </a>
           <p className="resorts__item-text">Александр Македонский основал Александрию в первой половине IV века до н.э. на берегу Средиземного моря. На протяжении многих веков, она была ключевым торговым центром для множества империй.</p>
           <a href="/country/egypt/alexandria/" className="resorts__item-link resorts__item-link--about">Подробнее о регионе</a>
@@ -96,7 +96,7 @@ const EgyptResorts = () => {
         </a>
         <div className="resorts__item-content">
           <a href="/country/egypt/cairo/" className="resorts__item-link">
-            <h3 className="resorts__item-title">Каир</h3>
+            <h3 id="cairo" className="resorts__item-title">Каир</h3>
           </a>
           <p className="resorts__item-text">Каир, известный как город тысячи минаретов, заслуживает этого названия, так как в его пределах расположено множество великолепных мечетей, которые являются архитектурным достоянием всего мира.</p>
           <a href="/country/egypt/cairo/" className="resorts__item-link resorts__item-link--about">Подробнее о регионе</a>
